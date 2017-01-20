ab249675ae2d7698103632512e71a135bbf3411d
DrawnBlue <DrawnBlu@gmail.com>
2017-01-20 07:56:56 -0800

    Commit All

--- BoringIdleGame/Form1.Designer.cs
+++ BoringIdleGame/Form1.Designer.cs
@@ -33,6 +33,11 @@
             this.btnButton = new System.Windows.Forms.Button();
             this.lblNumber = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblItem3Prc = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblItem3Lvl = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnItem3 = new System.Windows.Forms.Button();
             this.lblItem2Prc = new System.Windows.Forms.Label();
             this.lblItem2Lvl = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -50,8 +55,9 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.progressBarButton = new System.Windows.Forms.ProgressBar();
-            this.btnWin = new System.Windows.Forms.Button();
             this.timerProgressBar = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblAutoClick = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -70,6 +76,7 @@
             // 
             // btnButton
             // 
+            this.btnButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnButton.Font = new System.Drawing.Font("Coolvetica Rg", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnButton.Location = new System.Drawing.Point(13, 69);
             this.btnButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -94,6 +101,11 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblItem3Prc);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblItem3Lvl);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnItem3);
             this.groupBox1.Controls.Add(this.lblItem2Prc);
             this.groupBox1.Controls.Add(this.lblItem2Lvl);
             this.groupBox1.Controls.Add(this.label5);
@@ -112,6 +124,61 @@
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Purchase Items";
+            // 
+            // lblItem3Prc
+            // 
+            this.lblItem3Prc.AutoSize = true;
+            this.lblItem3Prc.Location = new System.Drawing.Point(380, 137);
+            this.lblItem3Prc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblItem3Prc.Name = "lblItem3Prc";
+            this.lblItem3Prc.Size = new System.Drawing.Size(49, 29);
+            this.lblItem3Prc.TabIndex = 14;
+            this.lblItem3Prc.Text = "500";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(303, 137);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 29);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Price:";
+            // 
+            // lblItem3Lvl
+            // 
+            this.lblItem3Lvl.AutoSize = true;
+            this.lblItem3Lvl.Location = new System.Drawing.Point(251, 137);
+            this.lblItem3Lvl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblItem3Lvl.Name = "lblItem3Lvl";
+            this.lblItem3Lvl.Size = new System.Drawing.Size(25, 29);
+            this.lblItem3Lvl.TabIndex = 12;
+            this.lblItem3Lvl.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(171, 137);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Level:";
+            // 
+            // btnItem3
+            // 
+            this.btnItem3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnItem3.Enabled = false;
+            this.btnItem3.Font = new System.Drawing.Font("Coolvetica Rg", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnItem3.Location = new System.Drawing.Point(7, 127);
+            this.btnItem3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnItem3.Name = "btnItem3";
+            this.btnItem3.Size = new System.Drawing.Size(156, 42);
+            this.btnItem3.TabIndex = 10;
+            this.btnItem3.Text = "Item 3";
+            this.toolTip1.SetToolTip(this.btnItem3, "Increases Auto Click Speed");
+            this.btnItem3.UseVisualStyleBackColor = true;
+            this.btnItem3.Click += new System.EventHandler(this.btnItem3_Click);
             // 
             // lblItem2Prc
             // 
@@ -155,6 +222,7 @@
             // 
             // btnItem2
             // 
+            this.btnItem2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnItem2.Enabled = false;
             this.btnItem2.Font = new System.Drawing.Font("Coolvetica Rg", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnItem2.Location = new System.Drawing.Point(7, 79);
@@ -210,6 +278,7 @@
             // btnItem1
             // 
             this.btnItem1.BackColor = System.Drawing.Color.Transparent;
+            this.btnItem1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnItem1.Enabled = false;
             this.btnItem1.Font = new System.Drawing.Font("Coolvetica Rg", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnItem1.Location = new System.Drawing.Point(7, 31);
@@ -236,6 +305,7 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.tabControl1.Location = new System.Drawing.Point(281, 70);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tabControl1.Name = "tabControl1";
@@ -261,7 +331,7 @@
             this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tabPage2.Size = new System.Drawing.Size(661, 532);
+            this.tabPage2.Size = new System.Drawing.Size(666, 499);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Buy More Stuff";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -277,29 +347,36 @@
             this.progressBarButton.TabIndex = 6;
             this.toolTip1.SetToolTip(this.progressBarButton, "Fill the bar for a bonus!");
             // 
-            // btnWin
-            // 
-            this.btnWin.Font = new System.Drawing.Font("Coolvetica Rg", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnWin.Location = new System.Drawing.Point(13, 544);
-            this.btnWin.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btnWin.Name = "btnWin";
-            this.btnWin.Size = new System.Drawing.Size(260, 47);
-            this.btnWin.TabIndex = 5;
-            this.btnWin.Text = "Click to Win";
-            this.btnWin.UseVisualStyleBackColor = true;
-            this.btnWin.Click += new System.EventHandler(this.btnWin_Click);
-            // 
             // timerProgressBar
             // 
             this.timerProgressBar.Interval = 10;
             this.timerProgressBar.Tick += new System.EventHandler(this.timerProgressBar_Tick);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 537);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(260, 58);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Hover over things to see\r\nwhat they do.\r\n";
+            // 
+            // lblAutoClick
+            // 
+            this.lblAutoClick.AutoSize = true;
+            this.lblAutoClick.Location = new System.Drawing.Point(13, 173);
+            this.lblAutoClick.Name = "lblAutoClick";
+            this.lblAutoClick.Size = new System.Drawing.Size(147, 29);
+            this.lblAutoClick.TabIndex = 8;
+            this.lblAutoClick.Text = "Auto Clicks: 0";
+            // 
             // Game
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(967, 640);
+            this.Controls.Add(this.lblAutoClick);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.progressBarButton);
-            this.Controls.Add(this.btnWin);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.lblNumber);
             this.Controls.Add(this.btnButton);
@@ -344,9 +421,15 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnItem2;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button btnWin;
         private System.Windows.Forms.ProgressBar progressBarButton;
         private System.Windows.Forms.Timer timerProgressBar;
+        private System.Windows.Forms.Label lblItem3Prc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblItem3Lvl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnItem3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblAutoClick;
     }
 }
 
--- BoringIdleGame/Form1.cs
+++ BoringIdleGame/Form1.cs
@@ -19,12 +19,15 @@ namespace BoringIdleGame
         }
 
         //Initialize Variables
+        //button
         int intButton = 1;
         int intNumber = 0;
 
+        //bonus bar
         decimal dbProgressAmount = 0;
         int intProgressGoal = 1;
 
+        //item 1
         int intItem1 = 0;
         int intItem1Lvl = 0;
         int intItem1Prc = 10;
@@ -32,6 +35,10 @@ namespace BoringIdleGame
         //item 2 is intButton
         int intItem2Lvl = 1;
         int intItem2Prc = 100;
+
+        int intItem3 = 0;
+        int intItem3Lvl = 0;
+        int intItem3Prc = 500;
 
 
         //form load
@@ -48,8 +55,10 @@ namespace BoringIdleGame
             intNumber += intButton;
             lblNumber.Text = Convert.ToString(intNumber);
 
-            if (dbProgressAmount < 1)
-            dbProgressAmount += .12m;
+            if (dbProgressAmount < 2)
+                dbProgressAmount += .126m;
+
+
         }
 
 
@@ -62,7 +71,73 @@ namespace BoringIdleGame
         //Item 2 Button (Button Upgrade
         private void btnItem2_Click(object sender, EventArgs e)
         {
-            Upgrade(ref intButton, 1, ref intItem2Prc, 1.75, ref intItem2Lvl, lblItem2Prc, lblItem2Lvl);
+            Upgrade(ref intButton, 1, ref intItem2Prc, 1.71, ref intItem2Lvl, lblItem2Prc, lblItem2Lvl);
+        }
+
+        //Item 3 Button
+        private void btnItem3_Click(object sender, EventArgs e)
+        {
+            Upgrade(ref intItem3, 1, ref intItem3Prc, 2.11, ref intItem3Lvl, lblItem3Prc, lblItem3Lvl);
+            timerNumberUpdater.Interval = Convert.ToInt32(timerNumberUpdater.Interval * 0.81);
+        }
+
+        //Numbers per second
+        private void timerNumberUpdater_Tick(object sender, EventArgs e)
+        {
+            intNumber += intItem1;
+            lblNumber.Text = Convert.ToString(intNumber);
+
+        }
+
+        //Label Updater
+        private void timerLabelUpdater_Tick(object sender, EventArgs e)
+        {
+            //Auto Clicks
+            lblAutoClick.Text = "Auto Clicks: " + intItem1;
+
+            //Item 1
+            btnItem1.Enabled = (intNumber >= intItem1Prc) ? true : false;
+
+            //Item 2
+            btnItem2.Enabled = (intNumber >= intItem2Prc) ? true : false;
+
+            //Item 3
+            btnItem3.Enabled = (intNumber >= intItem3Prc) ? true : false;
+        }
+
+
+        //Progress bar timer
+        private void timerProgressBar_Tick(object sender, EventArgs e)
+        {
+            if (dbProgressAmount > -10 && progressBarButton.Value > 1)
+            {
+                dbProgressAmount -= 0.0123m;
+            }
+
+            if (dbProgressAmount > 0)
+            {
+                if (progressBarButton.Value + Convert.ToInt32(dbProgressAmount) < progressBarButton.Maximum)
+                {
+                    progressBarButton.Value += Convert.ToInt32(dbProgressAmount);
+                }
+                else
+                {
+                    //dbProgressAmount = 0;
+                    progressBarButton.Value = 0;
+                    intNumber = Convert.ToInt32(Convert.ToDouble(intNumber) * 1.5);
+                }
+            }
+
+            if (dbProgressAmount < 0 && progressBarButton.Value + Convert.ToInt32(dbProgressAmount) > 0)
+            {
+                progressBarButton.Value += Convert.ToInt32(dbProgressAmount);
+            }
+            else if (progressBarButton.Value + Convert.ToInt32(dbProgressAmount) < 0)
+            {
+                progressBarButton.Value = 0;
+                dbProgressAmount = 0;
+            }
+
         }
 
         //upgrade method
@@ -78,64 +153,6 @@ namespace BoringIdleGame
             lblPrice.Text = Convert.ToString(Price);
 
             Item += ItemAmount;
-        }
-
-        //win box
-        private void btnWin_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Congratulations, you win!", "Yay!");
-        }
-
-        //Progress bar timer
-        private void timerProgressBar_Tick(object sender, EventArgs e)
-        {
-            if (dbProgressAmount > -2 && progressBarButton.Value > 1)
-            {
-                dbProgressAmount -= 0.0123m;
-            }
-
-            if (dbProgressAmount > 0)
-            {
-                if (progressBarButton.Value + Convert.ToInt32(dbProgressAmount) < progressBarButton.Maximum)
-                {
-                    progressBarButton.Value += Convert.ToInt32(dbProgressAmount);
-                }
-                else
-                {
-                    dbProgressAmount = 0;
-                    progressBarButton.Value = 0;
-                    intNumber = Convert.ToInt32(Convert.ToDouble(intNumber) * 1.5);
-                }
-            }
-            else if (dbProgressAmount < 0 && progressBarButton.Value + Convert.ToInt32(dbProgressAmount) > 0)
-            {
-                progressBarButton.Value += Convert.ToInt32(dbProgressAmount);
-            }
-            else if (progressBarButton.Value + Convert.ToInt32(dbProgressAmount) < 0)
-            {
-                progressBarButton.Value = 0;
-                dbProgressAmount = 0;
-            }
-
-        }
-
-        //Numbers per second
-        private void timerNumberUpdater_Tick(object sender, EventArgs e)
-        {
-            intNumber += intItem1;
-            lblNumber.Text = Convert.ToString(intNumber);
-
-        }
-
-        //Label Updater
-        private void timerLabelUpdater_Tick(object sender, EventArgs e)
-        {
-
-            //Item 1
-            btnItem1.Enabled = (intNumber >= intItem1Prc) ? true : false;
-
-            //Item 2
-            btnItem2.Enabled = (intNumber >= intItem2Prc) ? true : false;
         }
     }
 }
